728e2f47a291fd8a11d1dbf71baf493afc0aa09a
taufik0910 <taufik.cradle@gmail.com>
2023-03-02 18:25:00 +0700

    build feature Recommend Nearest Counter Page (AND-12) and List Services (AND-16) rev2

--- app/src/main/java/com/binaracademy/myaccountant/ui/main/MainActivity.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/main/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.binaracademy.myaccountant.ui.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.binaracademy.myaccountant.R
 
 import com.binaracademy.myaccountant.databinding.ActivityMainBinding
-import kotlin.math.log
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +26,10 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
         binding.toolbar.setupWithNavController(navController)
+//        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        supportActionBar?.setHomeButtonEnabled(false)
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
         ////        supportActionBar?.setDisplayHomeAsUpEnabled(false)
         //
         //        supportActionBar?.setHomeButtonEnabled(false)
