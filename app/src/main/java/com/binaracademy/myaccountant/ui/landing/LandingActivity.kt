dea377fb83f041644bd9acc98edc317386882515
jlianf <jalianfebriandy15@gmail.com>
2023-03-05 09:42:39 +0700

    AND-3

--- app/src/main/java/com/binaracademy/myaccountant/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/landing/LandingActivity.kt
@@ -12,5 +12,15 @@ class LandingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        val landingFragmentOne = LandingPageOneFragment()
+        val landingFragmentTwo = LandingPageTwoFragment()
+
+        val landingPageAdapter = LandingPagerAdapter(
+            fragmentManager = supportFragmentManager,
+            landingFragmentOne, landingFragmentTwo
+        )
+        binding.vpLanding.adapter = landingPageAdapter
+        binding.dotIndicator.attachTo(binding.vpLanding)
     }
 }
---
+++ app/src/main/java/com/binaracademy/myaccountant/ui/landing/LandingPagerAdapter.kt
@@ -0,0 +1,19 @@
+package com.binaracademy.myaccountant.ui.landing
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class LandingPagerAdapter(
+    fragmentManager: FragmentManager,
+    private vararg val fragment: Fragment,
+) : FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getCount(): Int {
+        return fragment.size
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return fragment[position]
+    }
+}
