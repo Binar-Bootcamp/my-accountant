4c691efa6a9a45a259e863a6716b0b9ee75a8cbb
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-07 18:38:52 +0700

    Fix Register

--- app/src/main/java/com/binaracademy/myaccountant/ui/income/IncomeActivity.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/income/IncomeActivity.kt
@@ -3,6 +3,8 @@ package com.binaracademy.myaccountant.ui.income
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.binaracademy.myaccountant.databinding.ActivityIncomeBinding
+import com.binaracademy.myaccountant.ui.main.MainActivity
+import com.binaracademy.myaccountant.util.helpers.intentTo
 
 class IncomeActivity : AppCompatActivity() {
 	
@@ -25,6 +27,7 @@ class IncomeActivity : AppCompatActivity() {
 					putString("gaji", income)
 					apply()
 				}
+				intentTo(MainActivity::class.java)
 			}
 		}
 	}
