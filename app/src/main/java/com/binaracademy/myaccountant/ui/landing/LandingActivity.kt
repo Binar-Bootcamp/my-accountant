a93e218255edf1e40add34ae5ac0c18c321d39fc
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-08 11:21:18 +0700

    AND-7

--- app/src/main/java/com/binaracademy/myaccountant/ui/landing/LandingActivity.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/landing/LandingActivity.kt
@@ -1,11 +1,14 @@
 package com.binaracademy.myaccountant.ui.landing
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.binaracademy.myaccountant.data.adapter.LandingPagerAdapter
 import com.binaracademy.myaccountant.databinding.ActivityLandingBinding
 import com.binaracademy.myaccountant.ui.main.MainActivity
 import com.binaracademy.myaccountant.ui.register.RegisterActivity
+import com.binaracademy.myaccountant.ui.splashscreen.SplashScreenActivity
 import com.binaracademy.myaccountant.util.helpers.Global
 import com.binaracademy.myaccountant.util.helpers.SharedPreferencesManager
 import com.binaracademy.myaccountant.util.helpers.intentTo
@@ -56,5 +59,55 @@ class LandingActivity : AppCompatActivity() {
 		)
 		binding.vpLanding.adapter = landingPageAdapter
 		binding.dotIndicator.attachTo(binding.vpLanding)
+	}
+	private val binding: ActivityLandingBinding by lazy {
+		ActivityLandingBinding.inflate(layoutInflater)
+	}
+	
+	private lateinit var sharePreference: SharedPreferences
+	private lateinit var editor: SharedPreferences.Editor
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		
+		setUpSharePreference()
+		
+		val landingFragmentOne = LandingPageOneFragment()
+		val landingFragmentTwo = LandingPageTwoFragment()
+		
+		val landingPageAdapter = LandingPagerAdapter(
+			this,
+			landingFragmentOne, landingFragmentTwo
+		)
+		binding.vpLanding.adapter = landingPageAdapter
+		binding.dotIndicator.attachTo(binding.vpLanding)
+		
+		binding.tvSkip.setOnClickListener {
+			intentTo(MainActivity::class.java)
+			finish()
+		}
+		
+		binding.btnVpNext.setOnClickListener {
+			val currentPosition = binding.vpLanding.currentItem
+			if (currentPosition == landingPageAdapter.itemCount - 1) {
+				setUpAction()
+				intentTo(RegisterActivity::class.java)
+				finish()
+			} else {
+				binding.vpLanding.setCurrentItem(currentPosition + 1, true)
+			}
+		}
+	}
+	
+	private fun setUpSharePreference() {
+		sharePreference =
+			this.getSharedPreferences(SplashScreenActivity.TABLE_DATA, Context.MODE_PRIVATE)
+		editor = sharePreference.edit()
+	}
+	
+	private fun setUpAction() {
+		editor.putBoolean(SplashScreenActivity.IS_FIRST, false)
+		editor.apply()
 	}
 }
--- app/src/main/java/com/binaracademy/myaccountant/ui/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/splashscreen/SplashScreenActivity.kt
@@ -1,19 +1,59 @@
 package com.binaracademy.myaccountant.ui.splashscreen
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.content.SharedPreferences.Editor
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.binaracademy.myaccountant.databinding.ActivitySplashScreenBinding
 import com.binaracademy.myaccountant.ui.landing.LandingActivity
+import com.binaracademy.myaccountant.ui.register.RegisterActivity
 import com.binaracademy.myaccountant.ui.register.RegisterActivity
 import com.binaracademy.myaccountant.util.helpers.Global
 import com.binaracademy.myaccountant.util.helpers.SharedPreferencesManager
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+	private val binding: ActivitySplashScreenBinding by lazy {
+		ActivitySplashScreenBinding.inflate(layoutInflater)
+	}
+	
+	private lateinit var sharePreference: SharedPreferences
+	private lateinit var editor: Editor
+	
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(binding.root)
+		
+		setUpSharePreference()
+		
+		val isFirst = sharePreference.getBoolean(IS_FIRST, true)
+		
+		Handler(Looper.getMainLooper()).postDelayed({
+			var i = if (isFirst){
+				Intent(this, LandingActivity::class.java)
+			} else {
+				Intent(this, RegisterActivity::class.java)
+			}
+			startActivity(i)
+			finish()
+		}, 2000)
+	}
+	
+	private fun setUpSharePreference() {
+		sharePreference = this.getSharedPreferences(TABLE_DATA, Context.MODE_PRIVATE)
+		editor = sharePreference.edit()
+	}
+	
+	companion object {
+		const val IS_FIRST = "IS_FIRST"
+		const val TABLE_DATA = "APP_TABLE"
+	}
 	private val binding: ActivitySplashScreenBinding by lazy {
 		ActivitySplashScreenBinding.inflate(layoutInflater)
 	}
