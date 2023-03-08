655eec046cc02e562e36b5915a50543d7c5a4637
Ryan Aprianto <ryan.aprianto77777@gmail.com>
2023-03-09 01:35:27 +0700

    Add NumberFormatter Rupiah

--- app/src/main/java/com/binaracademy/myaccountant/data/adapter/ListTransactionAdapter.kt
+++ app/src/main/java/com/binaracademy/myaccountant/data/adapter/ListTransactionAdapter.kt
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.binaracademy.myaccountant.R
 import com.binaracademy.myaccountant.data.enums.TransactionType
 import com.binaracademy.myaccountant.data.room.Transaction
+import com.binaracademy.myaccountant.util.helpers.NumberFormatter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import java.text.SimpleDateFormat
@@ -21,65 +22,67 @@ import kotlin.collections.ArrayList
 
 class ListTransactionAdapter(private val listTransaction: ArrayList<Transaction>) :
 	RecyclerView.Adapter<ListTransactionAdapter.ListViewHolder>() {
-
+	
 	private lateinit var onItemClickCallback: OnItemClickCallback
-
+	
 	@SuppressLint("NotifyDataSetChanged")
 	fun updateTransaction(transactions: List<Transaction>) {
 		this.listTransaction.clear()
 		this.listTransaction.addAll(transactions)
 		notifyDataSetChanged()
 	}
-
+	
 	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
 		val view: View =
 			LayoutInflater.from(parent.context).inflate(R.layout.item_transaction, parent, false)
 		return ListViewHolder(view)
 	}
-
+	
 	private fun getTransactionIcon(transactionType: TransactionType): Int {
 		return when (transactionType) {
 			TransactionType.INCOME -> {
-				Transaction.INCOME[ThreadLocalRandom.current().nextInt(0, Transaction.INCOME.size)]
+				Transaction.INCOME
 			}
 			TransactionType.OUTCOME -> {
-				Transaction.EXPENSE[ThreadLocalRandom.current().nextInt(0, Transaction.EXPENSE.size)]
+				Transaction.EXPENSE[ThreadLocalRandom.current()
+					.nextInt(0, Transaction.EXPENSE.size)]
 			}
 		}
 	}
-
+	
 	@RequiresApi(Build.VERSION_CODES.O)
 	override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
 		val transaction = listTransaction[position]
-
+		
+		val dateFormat = SimpleDateFormat("MMMM yyyy", Locale("id", "ID"))
+		val formattedDate = dateFormat.format(transaction.createdAt)
+		
 		transaction.type
-
+		
 		Glide.with(holder.itemView.context)
 			.load(getTransactionIcon(transaction.type))
 			.apply(RequestOptions().override(55, 55))
 			.into(holder.imgPhoto)
-
-		holder.tvCategory.text = transaction.source
+		
+		holder.tvCategory.text = if (transaction.source == "Initial") "Income" else transaction.source
 		holder.tvDescriptions.text = transaction.description
-		val dateFormat = SimpleDateFormat("MMMM yyyy", Locale("id", "ID"))
-		val formattedDate = dateFormat.format(transaction.createdAt)
 		holder.tvDate.text = formattedDate
-		holder.tvAmount.text = transaction.amount.toString()
+		holder.tvAmount.text = NumberFormatter.formatRupiah(transaction.amount)
 		holder.itemView.setOnClickListener { onItemClickCallback.onItemClick(listTransaction[holder.adapterPosition]) }
 	}
-
+	
 	override fun getItemCount(): Int {
 		return listTransaction.size
 	}
-
+	
 	fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
 		this.onItemClickCallback = onItemClickCallback
 	}
-
+	
 	interface OnItemClickCallback {
 		fun onItemClick(data: Transaction)
 	}
-
+	
 	inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 		var tvCategory: TextView = itemView.findViewById(R.id.tv_item_category)
 		var tvDescriptions: TextView = itemView.findViewById(R.id.tv_item_description)
--- app/src/main/java/com/binaracademy/myaccountant/data/room/Transaction.kt
+++ app/src/main/java/com/binaracademy/myaccountant/data/room/Transaction.kt
@@ -17,14 +17,7 @@ data class Transaction(
     @ColumnInfo(name = "created_at") var createdAt: Date = Date()
 ) {
     companion object {
-        val INCOME = intArrayOf(
-            R.drawable.ic_category_home_income,
-            R.drawable.ic_category_bill_income,
-            R.drawable.ic_category_shopping_income,
-            R.drawable.ic_category_entertaintment_income,
-            R.drawable.ic_category_social_income,
-            R.drawable.ic_category_vehicle_income
-        )
+        const val INCOME = R.drawable.ic_category_home_income
 
         val EXPENSE =  intArrayOf(
             R.drawable.ic_category_home_expense,
