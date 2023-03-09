766f7722dfedf588f4af6d0a5467cd166c8149bf
budivalid <budi@val.id>
2023-03-09 21:22:48 +0700

    fix: generate outcome icon

--- app/src/main/java/com/binaracademy/myaccountant/data/model/Transaction.kt
+++
@@ -1,9 +0,0 @@
-package com.binaracademy.myaccountant.data.model
-
-data class Transaction(
-	var category: String = "",
-	var description: String = "",
-	var date: String = "",
-	var amount: Long = 0L,
-	var photo: Int = 0
-)
