b235742f05840aef167ffd35813a697fce7e7981
budivalid <budi@val.id>
2023-03-10 18:12:53 +0700

    fix: list of history

--- app/src/main/java/com/binaracademy/myaccountant/data/presenter/HistoryContract.kt
+++ app/src/main/java/com/binaracademy/myaccountant/data/presenter/HistoryContract.kt
@@ -1,12 +1,12 @@
 package com.binaracademy.myaccountant.data.presenter
 
 import androidx.lifecycle.LiveData
-import com.binaracademy.myaccountant.data.room.Transaction
+import com.binaracademy.myaccountant.data.room.Summary
 
 interface HistoryContract {
 
     interface Presenter {
-        fun getHistoryTransaction(): LiveData<List<Transaction>>
+        fun getHistoryTransaction(): LiveData<List<Summary>>
     }
 
     interface View
--- app/src/main/java/com/binaracademy/myaccountant/ui/history/HistoryAdapter.kt
+++ app/src/main/java/com/binaracademy/myaccountant/ui/history/HistoryAdapter.kt
@@ -1,4 +1,5 @@
 package com.binaracademy.myaccountant.ui.history
+
 import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
@@ -6,59 +7,63 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.binaracademy.myaccountant.R
-import com.binaracademy.myaccountant.data.room.Transaction
+import com.binaracademy.myaccountant.data.room.Summary
 import com.binaracademy.myaccountant.databinding.ChildHistoryBinding
 import com.binaracademy.myaccountant.util.helpers.NumberFormatter
-import com.binaracademy.myaccountant.util.helpers.formatString
+import java.text.DateFormatSymbols
 import java.util.*
-import kotlin.collections.ArrayList
 
 
 @Suppress("UNREACHABLE_CODE")
 class HistoryAdapter(
-	private val listTransaction: ArrayList<Transaction>,
-	private val historyContext: Context
+    private val summaries: ArrayList<Summary>,
+    private val historyContext: Context
 ) :
-	RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
-	
-	private lateinit var onItemClickCallback: OnItemClickCallback
+    RecyclerView.Adapter<HistoryAdapter.HistoryViewHolder>() {
 
-	override fun onCreateViewHolder(parent : ViewGroup , viewType : Int) : HistoryViewHolder {
-		val view = ChildHistoryBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
-		return HistoryViewHolder(view)
-	}
-	
-	override fun getItemCount() : Int {
-		return listTransaction.size
-	}
-	
-	override fun onBindViewHolder(holder : HistoryViewHolder , position : Int) {
-		val transaction = listTransaction[position]
-		holder.grandTotal.text = NumberFormatter.formatRupiah(transaction.amount)
-		holder.totall.text =  NumberFormatter.formatRupiah(transaction.amount)
-		holder.bulan.text = transaction.createdAt.formatString("MMMM yyyy")
-		holder.itemView.setOnClickListener { onItemClickCallback.onItemClick(listTransaction[holder.adapterPosition]) }
-	}
+    private lateinit var onItemClickCallback: OnItemClickCallback
 
-	@SuppressLint("NotifyDataSetChanged")
-	fun updateHistory(transactions: List<Transaction>) {
-		this.listTransaction.clear()
-		this.listTransaction.addAll(transactions)
-		notifyDataSetChanged()
-	}
-	
-	fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
-		this.onItemClickCallback = onItemClickCallback
-	}
-	
-	interface OnItemClickCallback {
-		fun onItemClick(data: Transaction)
-	}
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoryViewHolder {
+        val view = ChildHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HistoryViewHolder(view)
+    }
 
-	inner class HistoryViewHolder(val binding : ChildHistoryBinding) :
-		RecyclerView.ViewHolder(binding.root) {
-		var bulan : TextView = binding.tvChildDate
-		var totall : TextView = binding.tvChildTotal
-		var grandTotal : TextView = binding.tvChildAmount
-	}
+    override fun getItemCount(): Int {
+        return summaries.size
+    }
+
+    override fun onBindViewHolder(holder: HistoryViewHolder, position: Int) {
+        val summary = summaries[position]
+        holder.grandTotal.text = NumberFormatter.formatRupiah(summary.budget)
+        holder.totall.text = NumberFormatter.formatRupiah(summary.total)
+        val monthYear = summary.id.split("-")
+
+        val monthNames: Array<String> = DateFormatSymbols(Locale("id", "ID")).months
+        val monthName = monthNames[monthYear.first().toInt() - 1]
+
+        holder.bulan.text = historyContext.getString(R.string.month_year, monthName, monthYear[1])
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClick(summaries[holder.adapterPosition]) }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateHistory(summaries: List<Summary>) {
+        this.summaries.clear()
+        this.summaries.addAll(summaries)
+        notifyDataSetChanged()
+    }
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClick(data: Summary)
+    }
+
+    inner class HistoryViewHolder(val binding: ChildHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        var bulan: TextView = binding.tvChildDate
+        var totall: TextView = binding.tvChildTotal
+        var grandTotal: TextView = binding.tvChildAmount
+    }
 }
